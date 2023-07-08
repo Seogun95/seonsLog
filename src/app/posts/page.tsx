9d6055277a06e216aa3cd03c44ff49267f49037b
Seogun95 <ksj083895@gmail.com>
2023-07-08 11:39:12 +0900

    feat: 마크다운의 matter 카테고리 별로 섹션을 나눠 맵핑

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,28 +1,53 @@
 import Link from 'next/link';
 
-import { getAllPosts } from '@/service/markdownData';
+import { getAllPosts, Post } from '@/service/markdownData';
+
+interface CategorizedPosts {
+  [category: string]: Post[];
+}
 
 export default async function BlogHomepage() {
   const allpost = await getAllPosts();
+  const categorizedPosts: CategorizedPosts = allpost.reduce(
+    (acc: CategorizedPosts, post: Post) => {
+      if (!acc[post.category]) {
+        acc[post.category] = [];
+      }
+      acc[post.category].push(post);
+      return acc;
+    },
+    // eslint-disable-next-line comma-dangle
+    {},
+  );
 
   return (
     <>
       <h1>블로그 홈</h1>
-      <ul>
-        {allpost.map((post, i) => (
-          <li key={i}>
-            <Link href={post.slug}>
-              <p>{post.title}</p>
-              <p>
-                {post.description.length > 30
-                  ? `${post.description.slice(0, 30)}...`
-                  : post.description}
-              </p>
-              <span>{post.date}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {/* category별로 나눠서 JSX에 나타냄 */}
+      {Object.entries(categorizedPosts).map(([category, posts]) => (
+        <>
+          <div key={category}>
+            <h2>{category}</h2>
+            <span>{posts.length}</span>
+            <ul>
+              {posts.map((post, i) => (
+                <li key={i}>
+                  <Link href={post.slug}>
+                    <p>{post.title}</p>
+                    <p>
+                      {post.description.length > 50
+                        ? `${post.description.slice(0, 50)}...`
+                        : post.description}
+                    </p>
+                    <span>{post.date}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <hr />
+        </>
+      ))}
     </>
   );
 }
