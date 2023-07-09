72e0c840760803f1f21e6629cc802903becb779f
Seogun95 <ksj083895@gmail.com>
2023-07-09 23:14:45 +0900

    feat: SSR데이터 함수파일로 리팩터링 후 , 컴포넌트 간소화

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,53 +1,24 @@
-import Link from 'next/link';
-
-import { getAllPosts, Post } from '@/service/markdownData';
-
-interface CategorizedPosts {
-  [category: string]: Post[];
-}
+import PostSectionTemp from '@/components/(posts)/PostSectionTemp';
+import {
+  categorizedPost,
+  getNextPosts,
+  getPlayPosts,
+} from '@/service/markdownData';
 
 export default async function BlogHomepage() {
-  const allpost = await getAllPosts();
-  const categorizedPosts: CategorizedPosts = allpost.reduce(
-    (acc: CategorizedPosts, post: Post) => {
-      if (!acc[post.category]) {
-        acc[post.category] = [];
-      }
-      acc[post.category].push(post);
-      return acc;
-    },
-    // eslint-disable-next-line comma-dangle
-    {},
-  );
+  const [nextFolder, playFolder] = await Promise.all([
+    getNextPosts(),
+    getPlayPosts(),
+  ]);
+  const getNextPost = categorizedPost(nextFolder);
+  const getPlayPost = categorizedPost(playFolder);
 
   return (
     <>
-      <h1>블로그 홈</h1>
-      {/* category별로 나눠서 JSX에 나타냄 */}
-      {Object.entries(categorizedPosts).map(([category, posts]) => (
-        <>
-          <div key={category}>
-            <h2>{category}</h2>
-            <span>{posts.length}</span>
-            <ul>
-              {posts.map((post, i) => (
-                <li key={i}>
-                  <Link href={post.slug}>
-                    <p>{post.title}</p>
-                    <p>
-                      {post.description.length > 50
-                        ? `${post.description.slice(0, 50)}...`
-                        : post.description}
-                    </p>
-                    <span>{post.date}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <hr />
-        </>
-      ))}
+      <h1>NextJS</h1>
+      <PostSectionTemp categoryData={getNextPost} type="list" />
+      <h1>잡담</h1>
+      <PostSectionTemp categoryData={getPlayPost} type="carousel" />
     </>
   );
 }
