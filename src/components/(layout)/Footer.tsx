0f8765eef2685534f3ccf4d9b6951e5424bd2a2e
Seogun95 <ksj083895@gmail.com>
2023-07-09 23:16:25 +0900

    style: footer height 값 수정

--- src/components/(layout)/Footer.tsx
+++ src/components/(layout)/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="w-full h-24 p-4 m-auto bg-slate-600">
-      <div>
+    <footer className="w-full h-10 p-4 m-auto bg-slate-600">
+      <div className="flex items-center h-full">
         <span>Copyright © 2023. 서근</span>
       </div>
     </footer>
