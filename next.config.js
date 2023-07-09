e2944fcd4c7465e36b8cce0c1a6730ce2612c128
Seogun95 <ksj083895@gmail.com>
2023-07-09 23:11:04 +0900

    chore: slider 패키지 설치 및 eslintrc 수정

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: '*.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'loremflickr.com',
       },
     ],
   },
